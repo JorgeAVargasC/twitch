6096ae71ef136694b52933ae52e731aca5ce2e30
JorgeVargas <javargas216@unicauca.edu.co>
2022-10-15 18:54:47 -0500

    Updates

---
+++ static/js/856.0f26963c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktwitch=self.webpackChunktwitch||[]).push([[856],{6856:function(t,n,a){a.d(n,{FU5:function(){return e}});var c=a(9983);function e(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}}]})(t)}}}]);
+//# sourceMappingURL=856.0f26963c.chunk.js.map
--- static/js/856.f0c125cf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkecommerce=self.webpackChunkecommerce||[]).push([[856],{6856:function(t,e,n){n.d(e,{FU5:function(){return c}});var a=n(9983);function c(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}}]})(t)}}}]);
-//# sourceMappingURL=856.f0c125cf.chunk.js.map
